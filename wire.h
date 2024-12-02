ae817c63a37ae81d34601b327b0d8b95e0903cdf
Mathieu <166537907+Sparthuus@users.noreply.github.com>
2024-12-02 14:51:53 +0100

    Update wire.h

--- wire.h
+++ wire.h
@@ -1,3 +1,10 @@
 /*Library*/
   #include <stdio.h>
   #include <stdlib.h>
+/*Useful*/
+  typedef struct _tree{
+    int           value;
+    struct _tree* pLeft;
+    struct _tree* pRight;
+    int           balance;
+} Tree;
