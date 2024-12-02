a8c7c73d16cb91446a3369ce6270997690205b56
habijey <habilanjeyarasa@gmail.com>
2024-12-02 11:40:18 +0100

    Update script_shell.sh
    i added some code to check the argument

--- script_shell.sh
+++ script_shell.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
+# check nb args wich need an adjustment
+if [ "$#" -ne 1 ] ; then
+    echo "Bad nb args !"
+    exit 1
+fi
+
+# check arg is a directory( if needed )
+if ! [ -d "$1" ] ; then
+    echo "'$1' is not a directory !"
+    exit 2
+fi
+
+file1=$1
+
+if ! [ -f "$file1" ] ; then
+    echo "$file1 does not exist !"
+    exit 1
+fi
+
 echo "hello"
