b3294cfcad8a3ddc7165e0fe48c538c0497b05b2
habijey <habilanjeyarasa@gmail.com>
2024-12-09 16:15:06 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,14 @@
 #include "avl.h"
-
+//finish with photo
 int main(){
-  int i,c,l; //v1 = id, v2 = 
+  int i,c,l; //v1 = id, v2 = capacity, v3= load
   int sum2 = 0;
   int sum3 = 0;
   int v1 = 0;
   while( scanf("%d,%d,%d\n", &i, &c, &l, ) == 3){
-      if(v1 !=i)
+      if(v1 !=i){
+        v1 = i;
+      }
     
     
       AVL *pRoot = createAVL(i,c,l);
