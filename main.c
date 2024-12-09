2784019528d67a3101c7015fdf8f30c2987bdbbd
judeown <168127128+judeown@users.noreply.github.com>
2024-12-09 15:15:59 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 #include "wire.h"
 
 int main(){
-  int v1,v2,v3,v4,v5,v6,v7,v8;
+  int v1,v2,v3,v4; //v1 = id, v2 = 
+  int sum2 = 0;
+  int sum3 = 0;
+  while( scanf("%d,%d,%d\n", &v1, &v2, &v3, ) == 3){
+
+
+
+  }
   
 
   
