e9e537b7f4375e3a87741ad7a1f386cf60988058
judeown <168127128+judeown@users.noreply.github.com>
2024-12-09 15:40:43 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,16 @@
 #include "avl.h"
 
 int main(){
-  int v1,v2,v3,v4; //v1 = id, v2 = 
+  int i,c,l; //v1 = id, v2 = 
   int sum2 = 0;
   int sum3 = 0;
-  while( scanf("%d,%d,%d\n", &v1, &v2, &v3, ) == 3){
+  while( scanf("%d,%d,%d\n", &i, &c, &l, ) == 3){
+      AVL *pRoot = createAVL(i,c,l);
+      if(pRoot == NULL){
+        exit(1);      
+      }
 
-      AVL *pRoot = createAVL(int i, long long c, long long l);
-
+    
   }
   
 
