687d7e5fa47908e1c79cda87a8faf4f9ac821af6
Mathieu <166537907+Sparthuus@users.noreply.github.com>
2024-12-12 12:41:15 +0100

    Update csv.c

--- PC/csv.c
+++ PC/csv.c
@@ -1,2 +1,18 @@
-
 #include <stdio.h>
+
+void open_file(const AVL* pNew, const char* namefile){
+  FILE* file = fopen(filename, "rb");
+  if (fichier == NULL) {
+        printf("Error when opening the File = %d\n", errno);
+        exit(1);
+    }
+  AVL* pRoot = malloc(sizeof(AVL);
+  fscanf(file, "%d; %d; %d; %d", pRoot->id, pRoot->capacity, pRoot->load);  
+     if (root == NULL) {
+                fclose(file);
+                free(file2);
+                return NULL;
+            } 
+
+  fclose(file);
+}
