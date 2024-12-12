a8e6f0e9da151f11da91028c4fe15e8284646db8
Mathieu <166537907+Sparthuus@users.noreply.github.com>
2024-12-12 13:25:37 +0100

    Update main.c

--- PC/main.c
+++ PC/main.c
@@ -1,10 +1,27 @@
 #include <avl.h>
 #include <csv.h>
 
+#include "avl.h"
+//finish with photo
 int main(){
-  d
-
-  
+  int i,c,l; //v1 = id, v2 = capacity, v3= load
+  int sum2 = 0;
+  int sum3 = 0;
+  int v1 = 0;
+  while( scanf("%d,%d,%d\n", &i, &c, &l, ) == 3){
+      if(v1 !=i){
+        v1 = i;
+      }
+    
+    
+      AVL *pRoot = createAVL(i,c,l);
+      if(pRoot == NULL){
+        exit(1);      
+      }
+      while 
+    
+  }
+ 
   free(file);
   return 0;
 }
