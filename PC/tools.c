b80e8add31e815db0e6cd0427bcc76e89c0f6128
Mathieu <166537907+Sparthuus@users.noreply.github.com>
2024-12-16 21:11:08 +0100

    Update tools.c

--- PC/tools.c
+++ PC/tools.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//Function to clear terminal 
 void clear(){
-    //Clear terminal 
-    printf("\033[2J\033[H");
+   printf("\033[2J\033[H");
 }
+
 void intro(){
   printf("  ██████╗██╗   ██╗    ███████╗██████╗ ███████╗\n"
          " ██╔════╝╚██╗ ██╔╝    ██╔════╝██╔══██╗██╔════╝\n"
@@ -17,22 +18,22 @@ void intro(){
 }
 
 void waiting_scene(){
-    int i = rand() % 3;
+    int i = 1 + rand() % 3;
     switch (i){
         case 1 :
-            printf("Almost finish...\n");
-            break
+            printf("Almost finished...\n");
+            break;
         case 2 :
-            printf("Does the time has time for the time ?\n");
-            break
+            printf("Does time has time for time ?\n");
+            break;
         case 3 :
-            printf("I think it's finish... right ?\n");
-            break
+            printf("I think it's finished... right ?\n");
+            break;
     }
 }
 
 void finish(){
-  printf(" Finish !\n");
+  printf(" Finished !\n");
 }
 
 
