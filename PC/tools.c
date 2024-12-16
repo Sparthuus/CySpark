b019b3687049af63ead35d1d2ff7570aacbe9d96
Mathieu <166537907+Sparthuus@users.noreply.github.com>
2024-12-16 15:55:59 +0100

    Update and rename intro.c to tools.c

--- PC/tools.c
+++ PC/tools.c
@@ -6,33 +6,34 @@ void effacer(){
     printf("\033[2J\033[H");
 }
 
+void continue(){
+    /*Fonction qui permet de faire une pause*/
+    printf("\033[1;30mPress \033[4mEnter\033[0m \033[1;30mto continue\033[0m\n");
+    #elif __linux__
+        int ch;
+        //initscr();
+        //cbreak();
+        //noecho();  
+        while((ch=getch())!='\n'); 
+        //endwin();         
+    #endif
+}
+
 void intro(){
-  int number = 1;
- 
   printf("  ██████╗██╗   ██╗    ███████╗██████╗ ███████╗\n"
          " ██╔════╝╚██╗ ██╔╝    ██╔════╝██╔══██╗██╔════╝\n"
          " ██║      ╚████╔╝     █████╗  ██║  ██║█████╗\n"  
          " ██║       ╚██╔╝      ██╔══╝  ██║  ██║██╔══╝\n"  
          " ╚██████╗   ██║       ███████╗██████╔╝██║\n"     
          "  ╚═════╝   ╚═╝       ╚══════╝╚═════╝ ╚═╝\n");
-  while (1){
   printf("\nWelcome to our programm Cy EDF !\n"
          " Please wait patiently\n");
-  }
-  effacer();
-  printf("    ___ _       _     _         _\n"
-         "   / __(_)_ __ (_)___| |__     / \\n"
-         "  / _\ | | '_ \| / __| '_ \   /  /\n"
-         " / /   | | | | | \__ \ | | | /\_/\n" 
-         " \/    |_|_| |_|_|___/_| |_| \/\n");   
-                                 
-
-
-  
 }
                                                   
-
-
-
+void finish(){
+  effacer();
+  printf(" Finish !\n");
+  continue();
+}
 
   
