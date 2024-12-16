eb9a5b8d26012d37c028d27d2c1765623760d5cc
Mathieu <166537907+Sparthuus@users.noreply.github.com>
2024-12-16 12:06:12 +0100

    Update csv.c

--- PC/csv.c
+++ PC/csv.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 
-void open_file(const AVL* pNew, const char* namefile){
+void open_file(const AVL* pNew, const char* filename){
   FILE* file = fopen(filename, "r+");
-  if (fichier == NULL) {
-        printf("Error when opening the File = %d\n", errno);
+  if (file == NULL) {
+        printf("Error when opening the File = %d\n", strerron(errno));
         exit(1);
     }
-    AVL* pRoot = malloc(sizeof(AVL);
-    fscanf(file, "%d; %d; %d; %d", pRoot->id, pRoot->capacity, pRoot->load);  
+    int id, capacity, load;
+    int height_change = 0 ;
+    while (fscanf(file, "%d; %d; %d; %d", &id, &capacity, &load)== 3) {
+      *pRoot = insertAVL(*pRoot, id, capacity, load, &height_change); 
+    }
     fclose(file);
+    printf("File successfully loaded into AVL tree\n");
     
 }
