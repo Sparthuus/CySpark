f7d33356a39a14603d4dffe585d739877a34f1c5
Mathieu <166537907+Sparthuus@users.noreply.github.com>
2024-12-16 14:53:50 +0100

    Update main.c

--- PC/main.c
+++ PC/main.c
@@ -6,10 +6,6 @@
 #include <stdio.h>
 //finish with photo
 int main(){
-  int i,c,l; //v1 = id, v2 = capacity, v3= load
-  int sum2 = 0;
-  int sum3 = 0;
-  int v1 = 0;
   AVLNode *root = NULL;
   const char *filename = "data.csv";
   int stationType;    // Variable to store the station type to analyze
