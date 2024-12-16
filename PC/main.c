cbd38cb742847305513e6119a419a2f2214647ea
Mathieu <166537907+Sparthuus@users.noreply.github.com>
2024-12-16 17:18:13 +0100

    Update main.c

--- PC/main.c
+++ PC/main.c
@@ -9,22 +9,22 @@ int main(){
       // Load data from the terminal
     printf("Loading data from the terminal...\n");
     int v1, v2, v3;
-    int sum2 = 0;
-    int sum3 = 0;
+    AVL* pRoot = NULL;
     while(scanf("%d %d %d", &v1, &v2, &v3) == 3){
       if (v1 == 0){
-          sum2 += v2;
-          sum3 += v3;
+          printf("Doesnt work");
       }
+      else {
+        pRoot = insertAVL();
     }
-    printf("0;%d;%d\n", sum2, sum3);
-    AVL* pRoot = NULL;
-    int totalload = calculateLoadSum(pRoot, stationType);
-    finish();
-    printf("The total load for stations of type %d is: %d\n", stationType, totalLoad);
-     // Display the AVL tree in order
     printf("Displaying the AVL tree in order:\n");
-    infix(pRoot);
+    infix(pRoot); // Display the AVL tree in order
+    finish();
+    freeAVL(pRoot);
+    int totalload = calculateLoadSum(pRoot, stationType);
+   
+    printf("The total load for stations of type %d is: %d\n", stationType, totalLoad);
+     
     printf("Sum of consumption by type of station :\n");
     /*
     AVLNode *root = NULL;
